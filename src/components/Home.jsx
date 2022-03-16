a1d80c11de68dc2eb0db37ed65fde43beff412e8
Malaya kumar <malayak68@gmail.com>
2022-03-16 16:36:35 +0530

    changes

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React  from "react";
+
+import { Navbar } from "./Navbar";
+
+
+export const  Home = () => {
+    return (
+     <>
+     <h2 >Welcome to Home Page</h2>
+     </>
+     )
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,33 @@
+import {Link} from "react-router-dom"
 const links = [
   {
     title: "Home",
     link: "/",
     id: "header-link-home",
   },
+  {
+    title: "About",
+    link:"/about",
+    id: "header-link-about"
+  },
+  {
+   title: "Movies",
+   link:"/movies",
+   id :"header-link-movies"
+  }
   //   add the other link as well
 ];
 export const Navbar = () => {
-  return {
+  return (
     //map through the link ad display it in header
-  };
+   
+   <div>
+     {
+       links.map((e) => (
+         <Link to={e.link}>{e.title}</Link>
+       ))
+     }
+   </div> 
+    
+  );
 };
